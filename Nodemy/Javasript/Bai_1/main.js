7b4233d2710e1f6cd50a1eec8f15d7cb3fe0cf66
Hiep <lehuyhiep449@gmail.com>
2021-03-26 08:38:34 +0700

    nodemy js

--- Nodemy/Javasript/Bai_1/main.js
+++ Nodemy/Javasript/Bai_1/main.js
@@ -44,7 +44,7 @@ for (let i = 0; i < array.length; i++) {
         console.log(element);
     }
 }
-console.log('----');
+console.log('lon hon 5');
 //in ra các số lơn hơn hoặc bằng 5 của mảng 
 for (let i = 0; i < array.length; i++) {
     const element = array[i];
@@ -52,13 +52,63 @@ for (let i = 0; i < array.length; i++) {
         console.log(element);
     }
 }
-console.log('----');
-//tổng của mảng
+console.log('tong cua mang');
 
-for (let i = 0; i < array.length; i++) {
+function tinhtong(array) {
     var temp = 0;
-    temp +=array[i];
-    console.log(temp);
+    for (let i = 0; i < array.length; i++) {
+        temp +=array[i];
+        console.log(temp);
+        
+    }
     return temp;
 }
-console.log('temp');
+console.log(tinhtong(array));
+console.log('tong cac so /5');
+
+function tinhtongcacsochiahetcho5(array) {
+    var temp = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 5 == 0 ) {
+            temp +=array[i];
+            console.log(temp);
+        }
+       
+        
+    }
+    return temp;
+}
+console.log(tinhtongcacsochiahetcho5(array));
+console.log('tich 3 so dau');
+function tich3sodau(array){
+    multiply = 1 ;
+    for (let i = 0; i < 3; i++) {
+        multiply  *= array[i] ;
+            console.log(multiply);
+    }
+    return multiply;
+}
+console.log(tich3sodau(array));
+console.log(array.sort());
+console.log('phan tu 5');
+function searchFive (){
+    
+}
+//bài 2
+console.log('bai 2');
+function dienTich(r){
+    return  Math.PI * Math.pow(r, 2 ) ;
+}
+console.log( dienTich(8)) ;
+console.log('bai 3');
+var obj = {
+    fistName:'Đỗ',
+    lastName:'Hao',
+}
+console.log(obj.fistName + ' ' + obj.lastName);
+console.log('bai 4');
+var car ={
+    hangXe:'BMW',
+    gia:300000,
+    xuatXu:'duc',
+}
