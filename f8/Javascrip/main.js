40d091fb71d0bab421110c231084158c583ba6a4
Hiep <lehuyhiep449@gmail.com>
2021-03-30 13:45:47 +0700

    add

--- f8/Javascrip/main.js
+++ f8/Javascrip/main.js
@@ -1,2 +1,33 @@
-var a = 5 + '5';
-console.log(typeof a);
+var courses =[
+    {
+        id: 1,
+        name:'javascript',
+        coin:200
+    },
+    {
+        id: 2,
+        name:'php',
+        coin:1
+    },
+    {
+        id: 3,
+        name:'ruby',
+        coin:1
+    },
+    {
+        id: 4,
+        name:'java',
+        coin:45
+    },
+    {
+        id: 5,
+        name:'reacts',
+        coin:23
+    }
+]
+//callback : hàm truyền vào dưới dạng tham số
+//duyệt qua từng phần tử của mảng chỉ cần 1 phần tử sẽ trả về true
+var isFree = courses.some(function(course ,index){
+    return course.coin === 0;
+});
+console.log(isFree);
