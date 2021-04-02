68386723d9e9db25fd8d4549dd125faa24a2f6be
Hiep <lehuyhiep449@gmail.com>
2021-04-03 01:05:42 +0700

    test

---
+++ Nodemy/Javasript/test/test.js
@@ -0,0 +1,100 @@
+// Bài 1:
+
+var response = [
+    {
+      id: "1",
+      createdAt: "2021-03-22T05:52:38.521Z",
+      name: "Bechtelar",
+      avatar:
+        "https://s3.amazonaws.com/uifaces/faces/twitter/IsaryAmairani/128.jpg",
+      content: "ddddd",
+      age: 19,
+    },
+    {
+      id: "2",
+      createdAt: "2021-03-22T03:46:01.125Z",
+      name: "Test",
+      [avatar]: "https://s3.amazonaws.com/uifaces/faces/twitter/gmourier/128.jpg",
+      content: "123456",
+      age: 22,
+    },
+    {
+      id: "3",
+      createdAt: "2021-03-21T23:56:41.225Z",
+      name: "Ludwig Morar",
+      avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/_shahedk/128.jpg",
+      content: "Personal Loan Account",
+      age: 20,
+    },
+    {
+      id: "4",
+      createdAt: "2021-03-23T07:19:07.853Z",
+      name: "Cicero Reichel",
+      avatar:
+        "https://s3.amazonaws.com/uifaces/faces/twitter/bolzanmarco/128.jpg",
+      content: "back up payment PCI",
+      age: 18,
+    },
+    {
+      id: "5",
+      createdAt: "2021-03-23T00:57:02.708Z",
+      name: "Myrl Bechtelar",
+      avatar:
+        "https://s3.amazonaws.com/uifaces/faces/twitter/sbtransparent/128.jpg",
+      content: "systems",
+      age: 30,
+    },
+    {
+      id: "6",
+      createdAt: "2021-03-22T16:29:38.808Z",
+      name: "Urban Rogahn",
+      avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/amayvs/128.jpg",
+      content: "enterprise",
+      age: 19,
+    },
+    {
+      id: "7",
+      createdAt: "2021-03-23T03:43:00.287Z",
+      name: "Jody Parisian",
+      avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/leehambley/128.jpg",
+      content: "Crescent Bedfordshire",
+      age: 18,
+    },
+    {
+      id: "8",
+      createdAt: "2021-03-23T06:07:12.431Z",
+      name: "Madeline Renner",
+      avatar:
+        "https://s3.amazonaws.com/uifaces/faces/twitter/jeremyworboys/128.jpg",
+      content: "24/365",
+      age: 29,
+    },
+    {
+      id: "9",
+      createdAt: "2021-03-22T23:13:36.792Z",
+      name: "Caroline Senger",
+      avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/kerihenare/128.jpg",
+      content: "ivory functionalities leverage",
+      age: 25,
+    },
+  ];
+  
+  // a) 0.5đ lấy đuôi của 1 file ảnh // .jpg
+  function file_Extension(file){
+    checkFile_Extension = file.map(function(x){
+      console.log(x.avatar);
+      return x[1];
+      // x = x.avatar.split('.') ;
+      // return x[x.length - 1 ];
+    });
+  
+    return checkFile_Extension;
+  }
+  
+  console.log(file_Extension(response));
+  // b) 0.5đ in ra object thông tin bài đăng của người tên 'Jody Parisian'
+  // c) 0.5đ sắp xếp theo thứ tự tuổi tăng dần
+  // d) 1đ sắp xếp theo thứ tự ngày đăng bài tăng dần
+  // e) 1đ In ra tên, tuổi của những người có tuổi bằng nhau
+  // [{name:'Cicero Reichel',age:18},{name:"Jody Parisian",age:18},
+  // {name:"Urban Rogahn",age:19},{name:'Bechtelar',age:19}]
--- f8/Javascrip/main.js
+++ f8/Javascrip/main.js
@@ -1,42 +1,5 @@
-var courses =[
-    {
-        id: 1,
-        name:'javascript',
-        coin:250
-    },
-    {
-        id: 2,
-        name:'php',
-        coin:330
-    },
-    {
-        id: 3,
-        name:'ruby',
-        coin:770
-    },
-    {
-        id: 4,
-        name:'java',
-        coin:0
-    },
-    {
-        id: 5,
-        name:'reacts',
-        coin:120
-    }
-]
-var i = 0
-function coinHandler(accumulator,currentValue,currentIndex,origan){
-    // i++
-    // console.table({
-    //     'lượt chạy' :i,
-    //     'Biến tích trứ' :accumulator
-    // });
-    // console.log(currentValue);
-    return accumulator + currentValue.coin;
-}
-// var totalCoin = courses.reduce(coinHandler, 0);
-var totalCoin = courses.reduce(function(a,b){
-    return a + b.coin;
-},0)
-console.log(totalCoin);
+var depthArray = [1,2,[3,4],5,6,[7,8,9]]
+var flatArray = depthArray.reduce(function(flatOutput,depthItems){
+    return flatOutput.concat(depthItems);
+},[]);
+console.log(flatArray);
