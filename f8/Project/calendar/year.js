3e0b64c82e882cb64e2f41fe443aefe464455259
Hiep <lehuyhiep449@gmail.com>
2021-04-21 03:17:54 +0700

    developing

--- f8/Project/calendar/year.js
+++ f8/Project/calendar/year.js
@@ -3,40 +3,10 @@ var year_current = date.getFullYear();
 var months_current = date.getMonth();
 const renderCalendar = function() {
 date.setDate(1);
-console.log(date.setDate(1))
+// console.log(date.setDate(1))
 //lấy ra days
 const monthDays = document.querySelectorAll(".days");
-  console.log(160,monthDays[1]);
-// ngày cuối  cùng trong tháng hiện tại
-const lastDay = new Date(
-  date.getFullYear(),
-  2,
-  0
-).getDate();
-console.log(166,lastDay);
- //ngày cuối cùng trong tháng trước
-const prevLastDay = new Date(
-  date.getFullYear(),
-  1,
-  0
-).getDate();
-  console.log(174,prevLastDay);
-  // vị trí mà ngày đầu tiên trong tháng hiển thị nghĩa là thứ ngày đầu tiên trong tháng hiển thị 
-const firstDayIndex = new Date(
-  date.getFullYear(),
-  2
-).getDay();
-  console.log(176,firstDayIndex)
-//vị trí hiển thị của ngày cuối cùng trong tháng hiện tại nghĩa là hiển thị ra thứ của ngày cuối cùng trong tháng hiện tại
-const lastDayIndex = new Date(
-  date.getFullYear(),
-  2,
-  0
-).getDay();
-console.log(184,lastDayIndex)
- // vị trí hiển thị của ngày đầu tiên trong tháng tiếp theo 
-const nextDays = 7 - lastDayIndex - 1;
-console.log(nextDays);
+  // console.log(160,monthDays[1]);
 const months = [
   "January",
   "February",
@@ -54,30 +24,63 @@ const months = [
 const monthArr = document.querySelectorAll(".date h1");
 let monthStoreArr =[]
 for (let i = 0; i < monthArr.length; i++) {
-   
-        // console.log(j);
-        console.log(55,monthArr[i])
+  
+        // console.log(55,monthArr[i])
         monthArr[i].innerHTML = months[i];
         monthStoreArr.push(i)
     
 }
-
-console.log(monthStoreArr)
-
+// console.log(monthStoreArr)
 const yearArr = document.querySelectorAll(".date p");
 for (let i = 0; i < yearArr.length; i++) {
 
     yearArr[i].innerHTML = date.getFullYear();
 }
 document.querySelectorAll(".date p").innerHTML = date.getFullYear();
+const block = document.querySelectorAll('.calendar');
+console.log(76,block);
+for (let g = 0; g < block.length; g++) {
+  console.log(43,g);
+  // ngày cuối  cùng trong tháng hiện tại
+const lastDay = new Date(
+  date.getFullYear(),
+  g + 1 ,
+  0
+).getDate();
+  console.log(50,lastDay);
+  //ngày cuối cùng trong tháng trước
+const prevLastDay = new Date(
+  date.getFullYear() ,
+  g,
+  0
+).getDate();
+  
+  console.log(57,prevLastDay);
+  // vị trí mà ngày đầu tiên trong tháng hiển thị nghĩa là thứ ngày đầu tiên trong tháng hiển thị 
+const firstDayIndex = new Date(
+  date.getFullYear(),
+  g 
+).getDay();
+  console.log(63,firstDayIndex)
+  //vị trí hiển thị của ngày cuối cùng trong tháng hiện tại nghĩa là hiển thị ra thứ của ngày cuối cùng trong tháng hiện tại
+const lastDayIndex = new Date(
+  date.getFullYear(),
+  g + 1,
+  0
+).getDay();
+console.log(70,lastDayIndex)
+ // vị trí hiển thị của ngày đầu tiên trong tháng tiếp theo 
+ const nextDays = 7 - lastDayIndex - 1;
+ console.log(nextDays);
+     
+ 
 
 let days = "";
 
 for (let x = firstDayIndex; x > 0; x--) {
   days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
-  for (let i = 0; i < monthDays.length; i++) {
-    monthDays[1].innerHTML=days;
-  }
+    monthDays[g].innerHTML=days;
+
 //   monthDays.innerHTML = days;
 }
 
@@ -87,25 +90,21 @@ for (let i = 1; i <= lastDay; i++) {
     date.getMonth() === new Date().getMonth()
   ) {
     days += `<div class="today">${i}</div>`;
-    for (let j = 0; j < monthDays.length; j++) {
-        monthDays[1].innerHTML=days;
-      }
+    monthDays[g].innerHTML=days;
     // monthDays.innerHTML = days;
   } else {
     days += `<div>${i}</div>`;
-    for (let j = 0; j < monthDays.length; j++) {
-        monthDays[1].innerHTML=days;
-      }
+    monthDays[g].innerHTML=days;
     // monthDays.innerHTML = days;
   }
 }
 
 for (let i = 1; i <= nextDays; i++) {
   days += `<div class="next-date">${i}</div>`;
-  for (let j = 0; j < monthDays.length; j++) {
-    monthDays[1].innerHTML=days;
-  }
+  monthDays[g].innerHTML=days;
 //   monthDays.innerHTML = days;
+}
+
 }
 };
 
