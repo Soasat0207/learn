8b20ca268cd83f46f9fa44f43c0b69271ed4cf5e
Hiep <lehuyhiep449@gmail.com>
2021-05-20 13:12:09 +0700

    form validate

---
+++ f8/Javascrip/validation_form/app.js
@@ -0,0 +1,20 @@
+Validator({
+    form:'#form-1',
+    errorSelector:'.form-message',
+    errorRed:'invalid',
+    rules:[
+        Validator.isRequired('#fullname','Vui lòng nhập trường này'),
+        Validator.isRequired('#email','Vui lòng nhập trường này'),
+        Validator.isEmail('#email','Trường này phải là email'),
+        Validator.isRequired('#password','Vui lòng nhập trường này'),
+        Validator.minLength('#password', `Mật khẩu phải dài 8 kí tự và chứ 1 kí tự in hoa 1 số và 1 kí tự đặc biệt`),
+        // Validator.isRequired('#password_confirmation','Vui lòng nhập trường này'),
+        // Validator.isConfirmed('#password_confirmation',function(){
+        //      return document.querySelector('#form-1 #password').value;
+        // },'Mật Khẩu cần trùng nhau')
+    ],
+    onSubmit:function(data){
+        console.log(data);
+        console.log(data.email);
+    }
+})
