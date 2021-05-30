84173fd12fb29cd7d4805670d74bf942a269d993
Hiepos_2017 <soasat020722000@gmail.com>
2021-05-30 16:47:07 +0700

    30/5

---
+++ Nodemy/express/router/mongodb.js
@@ -0,0 +1,103 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/K11_Nodemy',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false,
+    useCreateIndex:true,
+});
+const AccountSchema = new Schema({
+    username: String,
+    password: String,
+    email: String,
+    idPost: [{
+        type: String,
+        ref:'post'
+    }]
+},{
+    collection:'Account'
+});
+const PortSchema = new Schema({
+    title: String,
+    comment: String,
+    author: String,
+    idComment: [{
+        type: String,
+        ref:"comment"
+    }]
+},{
+    collection:'port'
+});
+const CommentSchema = new Schema({
+    content: String,
+    idAccount: {
+        type: String,
+        ref:'Account'
+    }
+},{
+    collection: "comment"
+})
+const AccountModel = mongoose.model('Account',AccountSchema);
+const PostModel = mongoose.model('port',PortSchema);
+const CommentModel = mongoose.model('comment',CommentSchema);
+// AccountModel.create({
+//     username: 'lehuyhiep',
+//     password: '123123',
+//     email: 'lehuyhiep449@gmail.com'
+// }),
+// PortModel.create({
+//     title: 'Hoc mongodb',
+//     comment: 'fightting',
+//     author: 'Hiep'
+// })
+// CommentModel.create({
+//     content: 'cố lên bạn ơi mình tin bạn lam được!'
+// })
+// AccountModel.findOneAndUpdate({
+//     _id:'60b2112598c6e365735fe22a',
+// },{
+//     idPost:[
+//         '60b2112598c6e365735fe22b',
+//     ]
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((error)=>{
+//     console.log(error);
+// })
+// PostModel.findOneAndUpdate({
+//     _id:'60b2112598c6e365735fe22b',
+// },{
+//     idComment:[
+//         '60b2112598c6e365735fe22c',
+//     ]
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((error)=>{
+//     console.log(error);
+// })
+// CommentModel.findOneAndUpdate({
+//     _id:'60b2112598c6e365735fe22c',
+// },{
+//     idUser:[
+//         '60b2112598c6e365735fe22a',
+//     ]
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((error)=>{
+//     console.log(error);
+// })
+AccountModel.findOne({
+    _id:'60b2112598c6e365735fe22a',
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error);
+})
