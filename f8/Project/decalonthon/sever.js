7a368dce48818689d43dbcefcbeb16a0764316a8
Hiep <lehuyhiep449@gmail.com>
2021-06-22 01:08:44 +0700

    update decalon thon

---
+++ f8/Project/decalonthon/sever.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const path = require('path');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'./public/html/index.html'))
+})
+app.get('/product_details', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/html/product_details.html'))
+})
+app.get('/list-product', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/html/list-product.html'))
+})
+app.get('/cart', (req, res) => {
+  res.sendFile(path.join(__dirname,'./public/html/cart.html'))
+})
+  
+app.use('/public', express.static(path.join(__dirname,'./public')))
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
